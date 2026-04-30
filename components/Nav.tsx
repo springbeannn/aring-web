0f2a89cfc6e78574aff4d6e5376c03013cf6dc57
springbeannn <letitdigit@gmail.com>
2026-04-30 21:04:43 +0900

    fix: Nav.tsx Fragment closing tag </>> to </>

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -83,7 +83,7 @@ export function TopNav() {
                             </div>
                     </div>
                     <SideMenu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
-              </>>
+              </>
             );
 }
 
