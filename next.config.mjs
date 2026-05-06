185002c286a759547fb1b4948c757fa5c479a907
의정뉴 <euijungner@gim-uijeong-ui-MacBookAir.local>
2026-05-06 15:20:11 +0900

    fix: Supabase 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'yycagcepyldhkkozommp.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
-
 export default nextConfig;
